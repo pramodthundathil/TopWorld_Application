3e5cb1fc95a6e6a67d967f769f3e6ff3fb9d8a6a
Pramod Gopinath <gopinath.pramod@gmail.com>
2022-11-13 18:09:22 +0530

    version 3.7

--- TopWorld/settings.py
+++ TopWorld/settings.py
@@ -77,17 +77,22 @@ WSGI_APPLICATION = 'TopWorld.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#       'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'TopHR',
+#         'USER': 'postgres',
+#         'PORT': 5432,
+#         'PASSWORD': 1234,
+#         'HOST': 'Localhost'
+#     }
+# }
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TopHR',
-        'USER': 'postgres',
-        'PORT': 5432,
-        'PASSWORD': 1234,
-        'HOST': 'Localhost'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
